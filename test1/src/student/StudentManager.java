62dcaa15b468209d13fbdf400aa2b45b44714f0b
may29 <may29@DESKTOP-705M17T>
2026-06-04 12:22:05 +0900

    주석 수정

--- test1/src/student/StudentManager.java
+++ test1/src/student/StudentManager.java
@@ -6,7 +6,6 @@ import java.util.Set;
 public class StudentManager {
     private Set<String> students = new HashSet<>();
 
-    //학생 추가
     public void addStudent(String name) {
         if (students.contains(name)) {
             throw new IllegalArgumentException("이미 존재하는 학생입니다: " + name);
